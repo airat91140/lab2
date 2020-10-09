2a279aa5060058c772a7ad2ed945b997d22e681a
Айрат Махмутов <airatmak@gmail.com>
2020-10-09 14:56:29 +0300

    hren

---
+++ lab_a/X16.h
@@ -0,0 +1,59 @@
+#ifndef LAB2_X16_H
+#define LAB2_X16_H
+
+#include <iostream>
+
+const int N = 10;
+
+using namespace std;
+
+void dopoln(const unsigned char [N], int, unsigned char [N]);
+
+int max(int, int);
+
+class X16 {
+private:
+    int len; //длина числа
+    unsigned char number[N] = {0};
+    //само число, в каждом байте - две цифры
+
+    void correctlen(); //корректирование длинны числа
+
+    inline char getsign(); //получение знака числа  0 - положительный, любой другой - отрицательный
+
+    void setmas(const unsigned char[N]); //установление своего массива
+
+public:
+    X16();//конструктор по умолчанию
+
+    explicit X16(long int num); //конструктор создающий число из константного значения
+
+    explicit X16(char *num); //конструктор создающий число из строки
+
+    ~X16();
+
+    ostream &print(ostream &); //вывод числа
+
+    istream &input(istream &); //ввод числа в виде строки
+
+    X16 add(X16); //сложение чисел
+
+    X16 subtract(X16); //вычетание чисел
+
+    X16 Lshift(int); //сдвиг влево на int
+
+    X16 Rshift(int); //сдвиг вправо на int
+
+    int getlen() const; //получение длины строки
+
+    const unsigned char (&getnumber())[N] { //получение самого числа как массива чисел
+        return number;
+    };
+
+    bool isEven(); //проверка на четность
+
+    int compare(X16); // сравнение числе  1 - левый больше, -1 - правый больше, 0 одинаковы
+};
+
+
+#endif //LAB2_X16_H
---
+++ lab_a/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "X16.h"
+#include "functions.h"
+
+using namespace std;
+
+int main() {
+    int rc;
+    char *errmsg = (char *) "";
+    X16 num;
+    cout << "Enter your number:" << endl;
+    (X16(20).add(X16(99080))).print(cout);
+    do {
+        cout << errmsg;
+        errmsg = (char *) "You are wrong. Repeat, please!\n";
+        try {
+            num.input(cin);
+            rc = 1;
+        }
+        catch (exception &br) {
+            cout << br.what() << endl;
+            rc = -1;
+        }
+    } while (rc < 0);
+    cout << "Choose your variant" << endl;
+    while ((rc = dialog(msgs, NMsgs))) {
+        if (!fptr[rc](num))
+            break;
+    }
+    return 0;
+}
